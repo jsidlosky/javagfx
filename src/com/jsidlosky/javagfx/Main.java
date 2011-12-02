49df066b100b005e889725a7d3feaa055428c822
Jeff Sidlosky <jsidlosky@gmail.com>
2011-12-02 15:35:39 -0800

    Main entry point of application

---
+++ src/com/jsidlosky/javagfx/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.jsidlosky.javagfx;
+
+/**
+ * @author jsidlosky
+ * 
+ */
+public class Main {
+
+	/**
+	 * The main entry point.
+	 * 
+	 * @param args
+	 *            the arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
