f5ef5bfc91ba6a49541bdbec61232f9f6e73bd50
Jeff Sidlosky <jsidlosky@gmail.com>
2011-12-03 13:39:56 -0800

    Reordered init/render/update methods

--- src/com/jsidlosky/javagfx/Game.java
+++ src/com/jsidlosky/javagfx/Game.java
@@ -14,7 +14,7 @@ import org.newdawn.slick.SlickException;
  * 
  */
 public class Game extends BasicGame {
-	
+
 	/**
 	 * Resources
 	 */
@@ -26,11 +26,11 @@ public class Game extends BasicGame {
 	 */
 	protected final int mapHeight = 2;
 	protected final int mapWidth = 2;
-	protected final int mapData[][] = {{0,0,0,0,0,0,1,1,1,0},
-									   {0,0,0,0,0,0,0,0,0,0},
-									   {0,0,0,0,0,0,0,0,0,0},
-									   {1,1,1,1,0,1,1,1,1,1}};
-	
+	protected final int mapData[][] = { { 0, 0, 0, 0, 0, 0, 1, 1, 1, 0 },
+										{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+										{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+										{ 1, 1, 1, 1, 0, 1, 1, 1, 1, 1 } };
+
 	/**
 	 * Player
 	 */
@@ -46,30 +46,8 @@ public class Game extends BasicGame {
 		super("javagfx");
 	}
 
-
 	public Game(String title) {
 		super(title);
-	}
-
-	/**
-	 * Render the current game state to the display.
-	 */
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException {
-		
-		final int scale = 1;
-		final int y_offset = 300; 
-		
-		// Render the map
-		for(int row = 0; row < mapData.length; row++) {
-			for(int col = 0; col < mapData[row].length; col++) {
-				if(mapData[row][col] > 0)
-					brick.draw(col * 16, row * 16 + y_offset, scale);
-			}
-		}
-		
-		// Render the player
-		player.draw(playerX * 16, playerY * 16 + y_offset, scale);
 	}
 
 	/**
@@ -80,11 +58,32 @@ public class Game extends BasicGame {
 		brick = new Image("res/brick.png");
 		player = new Image("res/player.png");
 	}
-
+	
 	/**
 	 * Update the game state.
 	 */
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
+	}
+
+	/**
+	 * Render the current game state to the display.
+	 */
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+
+		final int scale = 1;
+		final int y_offset = 300;
+
+		// Render the map
+		for (int row = 0; row < mapData.length; row++) {
+			for (int col = 0; col < mapData[row].length; col++) {
+				if (mapData[row][col] > 0)
+					brick.draw(col * 16, row * 16 + y_offset, scale);
+			}
+		}
+
+		// Render the player
+		player.draw(playerX * 16, playerY * 16 + y_offset, scale);
 	}
 }
